5bd8e57bcb92ef64b491977675e5d975f959a7a7
dishant90 <kalradishant03@gmail.com>
2017-05-06 23:04:56 +0530

    basic login functionality completed

--- src/main/java/com/tripleS/controller/StudentApplicationService.java
+++ src/main/java/com/tripleS/controller/StudentApplicationService.java
@@ -4,15 +4,20 @@ import java.sql.Date;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tripleS.model.EntityDetails;
 import com.tripleS.model.StudentFile;
+import com.tripleS.model.User;
 import com.tripleS.repository.StudentFileRepository;
+import com.tripleS.service.UserService;
 
 @Controller
 @RequestMapping("/studentApplication")
@@ -20,7 +25,27 @@ public class StudentApplicationService {
 
 	@Autowired
     StudentFileRepository studentFileRepository;
+	
+	@Autowired
+	private UserService userService;
     
+	@RequestMapping(value="/newCase", method = RequestMethod.GET)
+	public ModelAndView newCase(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("newCase");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/fragments/home", method = RequestMethod.GET)
+	public ModelAndView home(){
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmailID(auth.getName());
+		modelAndView.addObject("userName", user.getFirstName());
+		modelAndView.setViewName("fragments/home");
+		return modelAndView;
+	}
+	
     @RequestMapping(method=RequestMethod.POST)
     public @ResponseBody boolean createStudentFile() {
     	
